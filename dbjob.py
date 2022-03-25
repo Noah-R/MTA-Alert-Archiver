0b1fac3e9a4cfd4c27560658d4fe049e10467d6f
Noah Rutkovsky <noah.rutkovsky@gmail.com>
2022-03-25 19:02:37 -0400

    I guess pyodbc just doesn't work

--- dbjob.py
+++ dbjob.py
@@ -1,18 +1,17 @@
-import pyodbc
+import pymysql
 import alertfetcher
 
 server = "scratching-post.cc0rktzgr4u0.us-east-1.rds.amazonaws.com"
 database = "mydb"
-port = "3306"
 username = "admin"
 password = str(open("dbpw.txt").read())
 
 df = alertfetcher.fetch('https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/camsys%2Fsubway-alerts', writecsv=True)
 
-cnxn = pyodbc.connect("DRIVER={SQL Server};SERVER="+server+";PORT="+port+"DATABASE="+database+";UID="+username+";PWD="+password)#LIBMYSQL_ENABLE_CLEARTEXT_PLUGIN=y
-input("success!")
-cursor = cnxn.cursor()
+conn = pymysql.connect(host=server, user=username, passwd=password, db=database, connect_timeout=5)
+
+cur = conn.cursor()
 for index, row in df.iterrows():
-     cursor.execute("INSERT INTO Alerts (time, start, end, route, header, description) values(?, ?, ?,?,?)", row.time, row.start, row.end, row.route, row.header, row.description)
-cnxn.commit()
-cursor.close()
+     sql = "INSERT INTO Alerts (time, start, end, route, header, description) values(%s, %s, %s, %s,%s, %s)"
+     cur.execute(sql, (row.time, row.start, row.end, row.route, row.header, row.description))
+conn.commit()
