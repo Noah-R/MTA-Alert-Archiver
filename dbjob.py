92b00391888dbe722309431bbd5bd6b82b29c945
Noah Rutkovsky <noah.rutkovsky@gmail.com>
2022-03-25 17:15:43 -0400

    Here's where I leave it for now

--- dbjob.py
+++ dbjob.py
@@ -3,12 +3,13 @@ import alertfetcher
 
 server = "scratching-post.cc0rktzgr4u0.us-east-1.rds.amazonaws.com"
 database = "mydb"
+port = "3306"
 username = "admin"
 password = str(open("dbpw.txt").read())
 
 df = alertfetcher.fetch('https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/camsys%2Fsubway-alerts', writecsv=True)
 
-cnxn = pyodbc.connect("DRIVER={SQL Server};SERVER="+server+";DATABASE="+database+";UID="+username+";PWD="+password+"--enable-cleartext-plugin")#LIBMYSQL_ENABLE_CLEARTEXT_PLUGIN=y
+cnxn = pyodbc.connect("DRIVER={SQL Server};SERVER="+server+";PORT="+port+"DATABASE="+database+";UID="+username+";PWD="+password)#LIBMYSQL_ENABLE_CLEARTEXT_PLUGIN=y
 input("success!")
 cursor = cnxn.cursor()
 for index, row in df.iterrows():
